3afdf5b41229fa4dfcf7591f88e8456057752b39
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2020-09-18 19:14:14 +0300

    Added blocks, expect footer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$(function(){
+$(function () {
+  $('.fairy-tail__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+    autoplay: 3000,
+    fade: true
+  });
 
+  $('.our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+    autoplay: 3000,
+    fade: true
+  });
 });
