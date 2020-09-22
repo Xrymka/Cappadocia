9b664e23fffdf9eb76cfbf9d5921772d60d9a983
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2020-09-22 19:05:54 +0300

    Added Adaptive

--- js/app.js
+++ js/app.js
@@ -3,13 +3,38 @@ $(function () {
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
     autoplay: 3000,
-    fade: true
+    fade: true,
+
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.our-trip__slider').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
     autoplay: 3000,
-    fade: true
+    fade: true,
+
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $(".nav, .our-trip body").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
   });
 });
